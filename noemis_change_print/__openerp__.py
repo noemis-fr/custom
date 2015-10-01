d7b759935ffb91fcc57578a7387ef1d950f2747e
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-10-01 11:54:21 +0200

    [ADD] new empty module 'noemis_change_print';

---
+++ noemis_change_print/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Noemis - Change Print module for Odoo
+#    Copyright (C) 2015-Today Noemis (http://www.noemis.fr)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Noemis - Change Print',
+    'version': '1.0',
+    'category': 'Noemis - Custom',
+    'description': """
+Change default reporting
+========================
+
+* Add a new report on sale.order model, named 'pro-forma';
+    """,
+    'author': 'Noemis',
+    'website': 'http://www.noemis.fr',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+    ],
+}
