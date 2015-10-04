d8dde262af9fdf68250c24cf7a6309f423de1dd1
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-10-04 20:56:24 +0200

    [ADD] noemis_no_sale_warning module;

---
+++ noemis_no_sale_warning/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Noemis - No Sale Warning module for Odoo
+#    Copyright (C) 2015-Today Noemis (http://www.noemis.fr)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Noemis - No Sale Warning',
+    'version': '1.0',
+    'category': 'Noemis - Custom',
+    'description': """
+Remove Sale Warning if there is not enough stock
+================================================
+
+    """,
+    'author': 'Noemis',
+    'website': 'http://www.noemis.fr',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale_stock',
+    ],
+    'data': [
+    ],
+}
