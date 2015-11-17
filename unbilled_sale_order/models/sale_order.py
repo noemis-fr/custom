071ed34a3e581cfc16af6d7b3f1905b26ebd7cb3
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-11-18 00:26:45 +0100

    [WIP] unbilled_sale_order : first version without stored field;

--- unbilled_sale_order/models/sale_order.py
+++ unbilled_sale_order/models/sale_order.py
@@ -23,7 +23,7 @@ from openerp.osv.orm import Model
 from openerp.osv import fields
 
 import openerp.addons.decimal_precision as dp
-from openerp.addons.sale.sale import sale_order
+
 
 class sale_order(Model):
     _inherit = 'sale.order'
@@ -40,16 +40,56 @@ class sale_order(Model):
                     res[order.id] += line.price_subtotal
         return res
 
+    # Recompute Section
+    def _get_order_from_order_line(
+            self, cr, uid, ids, context=None):
+        print "_get_order_from_order_line"
+        order_ids = [x.order_id.id for x in self.browse(
+            cr, uid, ids, context=context)]
+        return list(set(order_ids))
+
     _columns = {
         'amount_untaxed_unbilled': fields.function(
             _compute_amount_untaxed_unbilled,
             string='Untaxed and Unbilled Amount',
             digits_compute=dp.get_precision('Account'),
-            store={
-                'sale.order': (
-                    lambda self, cr, uid, ids, c={}: ids, ['order_line'], 10),
-                'sale.order.line': (sale_order._get_order, [
-                    'price_unit', 'tax_id', 'discount', 'invoiced',
-                    'product_uom_qty'], 10),
-            }),
+#            store={
+#                'sale.order': (
+#                    lambda self, cr, uid, ids, c={}: ids, ['order_line'], 10),
+#                'sale.order.line': (_get_order_from_order_line, [
+#                    'price_unit', 'tax_id', 'discount', 'invoiced',
+#                    'product_uom_qty', 'invoice_lines'], 10),
+#            }
+            ),
+    }
+
+class sale_order_line(Model):
+    _inherit = 'sale.order.line'
+
+    # Compute Section
+    def _compute_amount_untaxed_unbilled(
+            self, cr, uid, ids, field_names=None, arg=False, context=None):
+        sol_obj = self.pool['sale.order.line']
+        res = {}
+        for order_line in self.browse(cr, uid, ids, context=context):
+            res[order_line.id] = order_line.price_subtotal
+            for invoice_line in order_line.invoice_lines:
+                if invoice_line.invoice_id.state not in ['draft', 'cancel']:
+                    res[order_line.id] -= invoice_line.price_subtotal
+        return res
+
+    _columns = {
+        'amount_untaxed_unbilled': fields.function(
+            _compute_amount_untaxed_unbilled,
+            string='Untaxed and Unbilled Amount',
+            digits_compute=dp.get_precision('Account'),
+##            store={
+##                'sale.order.line': (
+##                    lambda self, cr, uid, ids, c={}: ids, ['invoiced', 'invoice_lines'], 10),
+###                'sale.order.line': (_get_order_from_order_line, [
+###                    'price_unit', 'tax_id', 'discount', 'invoiced',
+###                    'product_uom_qty', 'invoice_lines'], 10),
+##            }
+            
+            ),
     }
