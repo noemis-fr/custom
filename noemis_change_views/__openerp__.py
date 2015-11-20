e16d34096f0d6bd61fcb0edd3757823bd5ba70a7
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-11-20 14:44:32 +0100

    [ADD] new module noemis_change_views;

---
+++ noemis_change_views/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Noemis - Custom Views Changes',
+    'version': '1.0',
+    'category': 'Custom',
+    'description': """
+    """,
+    'website': 'http://www.noemis.fr',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/view.xml',
+    ],
+}
