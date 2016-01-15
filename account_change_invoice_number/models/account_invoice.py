a1b41dbd4555e90a6c6c4010c547474cc2d63da2
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-01-15 02:43:16 +0100

    [ADD] new module to allow accounting people to rename an invoice;

---
+++ account_change_invoice_number/models/account_invoice.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    No Sale Warning module for Odoo
+#    Copyright (C) 2015-Today Noemis (http://www.noemis.fr)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields
+from openerp.osv.orm import Model
+
+
+class account_invoice(Model):
+    _inherit = 'account.invoice'
+
+    _columns = {
+        'number': fields.related(
+            'move_id', 'name', type='char', size=64, relation='account.move',
+            store=True, string='Number'),
+    }
