85be8524b017ff6f38f1c247600280c1d400d6f2
Florent THOMAS <florent.thomas@mind-and-go.com>
2016-10-26 13:03:28 +0200

    [FIX]Bad init reference [FIX]Bad model in report

--- picking_report_loan/report/__init__.py
+++ picking_report_loan/report/__init__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 
 
-import picking
+from . import picking
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
