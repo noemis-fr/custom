7f5b99f86f2f33d93cb4e4f32308e5ddc214d71f
Florent THOMAS <florent.thomas@mind-and-go.com>
2017-02-27 19:57:02 +0100

    Add filter on product ref in sale orders

---
+++ product_filter_for_orders/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Add filter on produt ref",
+    "summary": "Filter on sale_order product ref",
+    "version": "7.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://odoo-community.org/",
+    "author": "<AUTHOR(S)>, Florent THOMAS",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        
+        "views/sale_order_view.xml",
+    
+    ],
+    "demo": [
+        
+    ],
+    
+}
