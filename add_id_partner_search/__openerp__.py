8f14eac8062ac19cd70d298543b684bb929da122
gael.torrecillas <gael.torrecillas@mind-and-go.com>
2017-09-22 11:51:23 +0200

    add id in search partner

---
+++ add_id_partner_search/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'add id in search for res.partner',
+    'version': '1.0',
+    'category': 'Report',
+    'author': "Mind And Go",
+    'website': "http://www.mind-and-go.com",
+    'summary': ' ',
+    'sequence':99,
+    
+    'depends': ['base', 'base_util_refcodes'],
+    'data': [
+],
+    'qweb': [
+         ],
+    'installable': True,
+    'website': 'https://www.odoo.com/page/point-of-sale',
+    'auto_install': False,
+}
---
+++ add_id_partner_search/models/partner.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2015-Today Mind And Go (<https://mind-and-go.com>)
+#    Copyright (C) 2004 OpenERP SA (<http://www.openerp.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+import logging
+from openerp.osv.orm import Model
+from base_util_refcodes import name_tools
+
+_logger = logging.getLogger(__name__)
+   
+class ResPartner(Model):
+    _inherit = 'res.partner'
+    
+
+    def name_get(self, cr, uid, ids, context=None):
+
+        return name_tools.extended_name_get(self, cr, uid, ids,'[#%(id)s][%(ref)s] %(name)s', ['id','ref', 'name'], context=context)
+
+        # ^^^^^^^^^^^^^^^^^^^^ ^^^^^^^^^^^^^^^ # template mask field list
+    def name_search(self, cr, user, name='', args=None, operator='ilike', context=None, limit=100):
+        
+        return name_tools.extended_name_search(self, cr, user, name, args,operator, context=context, limit=limit, keys=['ref', 'name','id'])
+
+        # ^^^^^^^^^^^^^^^^^^^^ # field list to search
+
+
+    
+    
